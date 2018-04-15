309c612bc9ee5c3b8eccd61b28bb87b81ed4cdb3
BiggyBaron <dr.cleverest@gmail.com>
2018-04-16 04:10:50 +0600

    JQUERY done, bad solution

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, json, request
+import apiai
+import sys
+
+
 app = Flask(__name__)
+
+
+def textMessage(update):
+    request = apiai.ApiAI('167a082419914df2b44700f2bcda6087').text_request() # Токен API к Dialogflow
+    request.lang = 'ru' # На каком языке будет послан запрос
+    request.session_id = 'ZhuldyzAIbot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+    request.query = update # Посылаем запрос к ИИ с сообщением от юзера
+    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    response = responseJson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
+    # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+    if response:
+        print("Действие: " + str(responseJson['result']['action']))
+        print("Ответ: " + str(responseJson['result']['fulfillment']['speech']))
+        print("Вероятность: " + str(responseJson['result']['score']*100))
+        return str(responseJson['result']['fulfillment']['speech'])
+    else:
+        print("Непонятно")
+        return "Простите, я не поняла Вас"
 
 
 @app.route('/')
@@ -10,9 +32,16 @@ def index():
 
 @app.route('/get_len', methods=['GET', 'POST'])
 def get_len():
-    #name = request.form['name']
-    name = input("Passs: ")
-    return json.dumps({'len': str(name), 'path': str("/static/happy.png")})
+    #name = request.speechText["saidText"]
+    #name = input("Passs: ")
+    if request.form['text']:
+        print(request.form['text'])
+        phrase2 = textMessage(request.form['text'])
+    else:
+        phrase2 = ''
+    name = "Happy"
+    mood = name
+    return json.dumps({'face': 1, 'len': str(name), 'path': str("/static/" + str(mood) + ".png"), 'phrase': phrase2})
 
 
 if __name__ == '__main__':
